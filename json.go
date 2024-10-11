75c7188bd2ebae26ef7a26941663ec733b9d69c2
AndreasNoebbe <andreasfabian2010@hotmail.com>
2024-10-11 10:56:05 +0200

    Fix security issues

--- json.go
+++ json.go
@@ -27,9 +27,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	// Handle the error from w.Write
 	_, err = w.Write(dat)
 	if err != nil {
 		log.Printf("Critical error writing response: %s", err)
-    }
+	}
 }
